4ed1e32589945affdf8dbb735184660783d8917b
Ryan Modrak <rmodrak@lanl.gov>
2020-07-29 00:08:26 -0600

    Added 2D examples

---
+++ 2d/simple-coordinate-system/parameters.py
@@ -0,0 +1,52 @@
+
+WORKFLOW='inversion'    # inversion, migration
+SOLVER='specfem2d'      # specfem2d, specfem3d
+SYSTEM='multicore'      # serial, pbs, slurm
+OPTIMIZE='LBFGS'        # base, newton
+PREPROCESS='base'       # base
+POSTPROCESS='base'      # base
+
+MISFIT='Waveform'
+MATERIALS='LegacyAcoustic'
+DENSITY='Constant'
+
+
+# WORKFLOW
+BEGIN=1                 # first iteration
+END=5                   # last iteration
+NREC=30                 # number of receivers
+NSRC=28                 # number of sources
+SAVEGRADIENT=1          # save gradient how often
+
+
+# PREPROCESSING
+FORMAT='su'             # data file format
+CHANNELS='y'            # data channels
+NORMALIZE=0             # normalize
+BANDPASS=0              # bandpass
+FREQLO=0.               # low frequency corner
+FREQHI=0.               # high frequency corner
+
+
+# POSTPROCESSING
+SMOOTH=20.              # smoothing radius
+SCALE=6.0e6             # scaling factor
+
+
+# OPTIMIZATION
+PRECOND=None            # preconditioner type
+STEPMAX=10              # maximum trial steps
+STEPTHRESH=0.1          # step length safeguard
+
+
+# SOLVER
+NT=4800                 # number of time steps
+DT=0.06                 # time step
+F0=0.084                # dominant frequency
+
+
+# SYSTEM
+NTASK=28                 # must satisfy 1 <= NTASK <= NSRC
+NPROCMAX=7
+NPROC=1                 # processors per task
+
---
+++ 2d/simple-coordinate-system/paths.py
@@ -0,0 +1,13 @@
+
+DATA = ''
+
+MODEL_INIT = '/Users/rmodrak/devel/scal/2d/simple-coordinate-system/specfem2d-d745c542/model_homogeneous'
+
+MODEL_TRUE = '/Users/rmodrak/devel/scal/2d/simple-coordinate-system/specfem2d-d745c542/model_coarse'
+
+PRECOND = ''
+
+SPECFEM_DATA = '/Users/rmodrak/devel/scal/2d/simple-coordinate-system/specfem2d-d745c542/DATA'
+
+SPECFEM_BIN = '/Users/rmodrak/devel/scal/2d/simple-coordinate-system/specfem2d-d745c542/bin'
+
